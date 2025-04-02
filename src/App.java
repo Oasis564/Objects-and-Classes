5f3f17369e4923c86aa131f9db328f65cac5678b
osaeid rony <osaeidfire@gmail.com>
2025-04-02 18:54:18 +0100

    marks and profiles

--- src/App.java
+++ src/App.java
@@ -13,6 +13,22 @@ public class App {
         System.out.println("Phone-Number: "+osaeidsProfile.phoneNumber);
         System.out.println("Email: "+osaeidsProfile.email);
         System.out.println("==========================================");
+
+        MarkSheet osaeidMarkSheet = new MarkSheet();
+        osaeidMarkSheet.maths = 90;
+        osaeidMarkSheet.english = 90;
+        osaeidMarkSheet.history = 89;
+        //calculate the total score and percentage
+        double totalMarkScore = osaeidMarkSheet.maths + osaeidMarkSheet.english + osaeidMarkSheet.history;
+        //the formula of a percentage is (marks. / total marks)*100
+        double percentage = (totalMarkScore/300)*100;
         
+
+        System.out.println("Maths Score: " + osaeidMarkSheet.maths);
+        System.out.println("English Score: " + osaeidMarkSheet.english);
+        System.out.println("History Score: " + osaeidMarkSheet.history);
+        System.out.println("Total Marks: " + totalMarkScore);
+        System.out.println("Percentage in all subjects: " + percentage + "%");
+        System.out.println("==========================================");
     }
 }
